3bd4a039e0372ecac9b2cd97ad4eb8a9544adaf6
Kunjan Patel <82031083+Kunjan-198@users.noreply.github.com>
2022-07-25 18:53:47 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -19,32 +19,32 @@
 # 	sub =  rospy.Subscriber('cmd_vel', Twist, callback)
 #     time.sleep(3)	
 
-# import serial,time
-# if __name__ == '__main__':
+import serial,time
+if __name__ == '__main__':
     
-#     print('Running. Press CTRL-C to exit.')
-#     with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
-#         time.sleep(0.1) #wait for serial to open
-#         if arduino.isOpen():
-#             print("{} connected!".format(arduino.port))
-#             try:
-#                 while True:
-#                     cmd=input("Enter command : ")
-#                     arduino.write(cmd.encode())
-#                     #time.sleep(0.1) #wait for arduino to answer
-#                     while arduino.inWaiting()==0: pass
-#                     if  arduino.inWaiting()>0: 
-#                         answer=arduino.readline()
-#                         print(answer)
-#                         arduino.flushInput() #remove data after reading
-#             except KeyboardInterrupt:
-#                 print("KeyboardInterrupt has been caught.")
-import serial
+    print('Running. Press CTRL-C to exit.')
+    with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+        time.sleep(0.1) #wait for serial to open
+        if arduino.isOpen():
+            print("{} connected!".format(arduino.port))
+            try:
+                while True:
+                    cmd=input("Enter command : ")
+                    arduino.write(cmd.encode())
+                    #time.sleep(0.1) #wait for arduino to answer
+                    while arduino.inWaiting()==0: pass
+                    if  arduino.inWaiting()>0: 
+                        answer=arduino.readline()
+                        print(answer)
+                        arduino.flushInput() #remove data after reading
+            except KeyboardInterrupt:
+                print("KeyboardInterrupt has been caught.")
+# import serial
 
-ser = serial.Serial('/dev/ttyACM0',9600)
-s = [0,1]
-while True:
-	read_serial=ser.readline()
-	s[0] = str(int (ser.readline(),16))
-	print s[0]
-	print read_serial
+# ser = serial.Serial('/dev/ttyACM0',9600)
+# s = [0,1]
+# while True:
+# 	read_serial=ser.readline()
+# 	s[0] = str(int (ser.readline(),16))
+# 	print s[0]
+# 	print read_serial
