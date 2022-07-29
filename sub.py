4a87f242f2259a03924e20c0a7a10f6fc67c6bb1
Kunjan Patel <82031083+Kunjan-198@users.noreply.github.com>
2022-07-29 15:44:21 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,25 +1,5 @@
-#!/usr/bin/env python3
-import rospy
-from std_msgs.msg import Float64
-from geometry_msgs.msg import Twist
+#!/usr/bin/env python
 import serial,time
-
-def callback(msg):
-	print (msg)
-	left=msg.linear.x+(msg.angular.z)
-	right=msg.linear.x-(msg.angular.z)
-	if(abs(left)>1.5):
-	    left=(left/abs(left))*1.5
-	if(abs(right)>1.5):
-	    right=(right/abs(right))*1.5
-	PWM1 = (left/1.5)*255
-	PWM2 = (right/1.5)*255
-        arduino.write(PWM1.encode())
-	time.sleep(1)
-	arduino.write(PWM2.encode())
-	time.sleep(1)
-
-rospy.init_node('Publisher')	
 if __name__ == '__main__':
     
     print('Running. Press CTRL-C to exit.')
@@ -29,7 +9,8 @@ if __name__ == '__main__':
             print("{} connected!".format(arduino.port))
             try:
                 while True:
-		    sub =  rospy.Subscriber('cmd_vel', Twist, callback)
+                    cmd=input("Enter command : ")
+                    arduino.write(cmd.encode())
             except KeyboardInterrupt:
                 print("KeyboardInterrupt has been caught.")
 
