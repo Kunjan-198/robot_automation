8ec91e61f159d49df971984ea847004941a9ab93
Kunjan Patel <82031083+Kunjan-198@users.noreply.github.com>
2022-07-29 16:04:05 +0530

    Update chali_ja_please.py

--- chali_ja_please.py
+++ chali_ja_please.py
@@ -5,23 +5,23 @@ from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
 
 def callback(msg):
-	print (msg)
-	left=msg.linear.x+(msg.angular.z)
-	right=msg.linear.x-(msg.angular.z)
-	if(left>1.5):
-	    left=1.5
-    elif(left<-1.5):
-        left=-1.5
-	if(right>1.5):
-	    right=1.5
-    elif(right<-1.5):
-        right=-1.5
-	PWM1 = ((left+1.5)/3)*512
-	PWM2 = ((right+1.5)/3)*512
-    arduino.write(PWM1.encode())
-	time.sleep(1)
-	arduino.write(PWM2.encode())
-	time.sleep(1)
+        print (msg)
+        left=msg.linear.x+(msg.angular.z)
+        right=msg.linear.x-(msg.angular.z)
+        if(left>1.5):
+            left=1.5
+        elif(left<-1.5):
+            left=-1.5
+        if(right>1.5):
+            right=1.5
+        elif(right<-1.5):
+            right=-1.5
+        PWM1 = ((left+1.5)/3)*512
+        PWM2 = ((right+1.5)/3)*512
+        arduino.write(PWM1.encode())
+        time.sleep(1)
+        arduino.write(PWM2.encode())
+        time.sleep(1)
 
 if __name__ == '__main__':
     
